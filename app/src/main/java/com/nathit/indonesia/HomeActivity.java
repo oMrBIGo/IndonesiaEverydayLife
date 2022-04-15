d5bb60e8aae4425f5b3f999248e7f0634b4af8c5
oMrBIGo <nathit.panrod1@gmail.com>
2022-04-15 16:32:29 +0700

    Developer by Nathit Panrod

--- app/src/main/java/com/nathit/indonesia/HomeActivity.java
+++ app/src/main/java/com/nathit/indonesia/HomeActivity.java
@@ -2,13 +2,46 @@ package com.nathit.indonesia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+    private long backPressedTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        Button btn_logout = findViewById(R.id.btn_logout);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+                startActivity(new Intent(HomeActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            moveTaskToBack(true);
+            finish();
+        } else {
+            Toast.makeText(this, "ทำอีกครั้งเพื่อออกจากแอปพลิเคชัน", Toast.LENGTH_SHORT).show();
+        }
+        backPressedTime = System.currentTimeMillis();
     }
 }
--- app/src/main/java/com/nathit/indonesia/MainActivity.java
+++ app/src/main/java/com/nathit/indonesia/MainActivity.java
@@ -6,13 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+    private long backPressedTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         Button btn_register = (Button) findViewById(R.id.btn_register);
         Button btn_login = (Button) findViewById(R.id.btn_login);
@@ -32,5 +41,29 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (firebaseAuth.getCurrentUser() != null) {
+            Toast.makeText(MainActivity.this, "เข้าสู่ระบบแล้ว!", Toast.LENGTH_SHORT).show();
+            //Start the HomeActivity
+            startActivity(new Intent(MainActivity.this, HomeActivity.class));
+            finish();
+        } else {
+            Toast.makeText(MainActivity.this, "ยินดีต้อนรับเข้าสู่แอปพลิเคชัน", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            moveTaskToBack(true);
+            finish();
+        } else {
+            Toast.makeText(this, "ทำอีกครั้งเพื่อออกจากแอปพลิเคชัน", Toast.LENGTH_SHORT).show();
+        }
+        backPressedTime = System.currentTimeMillis();
     }
 }
