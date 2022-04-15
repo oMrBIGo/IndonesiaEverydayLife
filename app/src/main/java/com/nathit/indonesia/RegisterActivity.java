c1696456e28e36ff807a8fff0fa7c415a9886226
oMrBIGo <nathit.panrod1@gmail.com>
2022-04-15 14:13:51 +0700

    Developer by Nathit Panrod

--- app/src/main/java/com/nathit/indonesia/RegisterActivity.java
+++ app/src/main/java/com/nathit/indonesia/RegisterActivity.java
@@ -4,7 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class RegsterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
